83a78cafe7d689b83ab68c52b762897c48ee2a2e
xistri <cristian93marti@gmail.com>
2023-02-10 19:10:33 +0100

    add context provider to index

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './core/components/app/App';
+import { ExerciseContextProvider } from './core/context/exercises/exercise.provider';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -9,9 +10,11 @@ const root = ReactDOM.createRoot(
 );
 root.render(
     <React.StrictMode>
-        <Router>
-            <App />
-        </Router>
+        <ExerciseContextProvider>
+            <Router>
+                <App />
+            </Router>
+        </ExerciseContextProvider>
     </React.StrictMode>
 );
 
