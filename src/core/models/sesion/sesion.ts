ae7eb0a106400f5492f77d63e1953fbe312c21d9
xistri <cristian93marti@gmail.com>
2023-02-10 18:51:55 +0100

    fix sesion model

--- src/core/models/sesion/sesion.ts
+++ src/core/models/sesion/sesion.ts
@@ -4,13 +4,13 @@ import { ExerciseStructure } from '../exercise/exercise';
 export type sesionStructure = {
     id: string;
     name: string;
-    exercises: Array<ExerciseStructure>;
+    exercises: Array<ExerciseStructure['id']>;
 };
 
 export class Sesion implements sesionStructure {
     public id: string;
     public name: string;
-    constructor(public exercises: Array<ExerciseStructure>) {
+    constructor(public exercises: Array<ExerciseStructure['id']>) {
         this.id = generateId();
         this.name = 'Sesion';
     }
