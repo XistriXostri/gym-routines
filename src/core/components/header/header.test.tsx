b7f1e96fe53d55e8d51b5714c890cc7b578d8f36
xistri <cristian93marti@gmail.com>
2023-02-17 07:44:44 +0100

    add header test

--- src/core/components/header/header.test.tsx
+++ src/core/components/header/header.test.tsx
@@ -1,44 +1,118 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
+import { useRoutines } from '../../hooks/use.routines';
+import { useUser } from '../../hooks/use.user';
+import { mockDefaultExercises } from '../../mocks/default.exercises.mock';
+import {
+    mockRoutinesEditing,
+    mockRoutinesNotEditing,
+} from '../../mocks/routines.mock';
+import { createPreloadedState, createStoreMock } from '../../mocks/store.mock';
+import { mockHandleEditMode } from '../../mocks/use.routines.mock';
+import { mockEmptyUser, mockUser, userMock } from '../../mocks/user.mock';
 import { Header } from './header';
 
-//TODO: tal vez no iba por estos mocks
-jest.mock('../../hooks/use.routines', () => ({
-    useRoutines: () => ({
-        handleEditMode: jest.fn(),
-        routinesState: {
-            isEditing: false,
-        },
-    }),
-}));
-
-jest.mock('../../hooks/use.user', () => ({
-    useUser: () => ({
-        userState: {
-            user: null,
-        },
-    }),
-}));
+jest.mock('../../hooks/use.user');
+jest.mock('../../hooks/use.routines');
 
 describe('Header', () => {
-    test('does not display back button when user is not logged in', () => {
-        render(<Header />);
-        const backButtonElement = screen.queryByRole('link');
-        expect(backButtonElement).toBeNull();
+    describe('when there isnt any user logged in', () => {
+        const mockPreloadStateEmptyUser = createPreloadedState(
+            mockEmptyUser,
+            mockRoutinesNotEditing,
+            mockDefaultExercises
+        );
+
+        const mockStoreEmptyUser = createStoreMock(mockPreloadStateEmptyUser);
+
+        test('does not display back button', () => {
+            (useUser as jest.Mock).mockReturnValue({
+                userState: {
+                    user: null,
+                },
+            });
+
+            (useRoutines as jest.Mock).mockReturnValue({
+                routinesState: mockRoutinesEditing,
+                handleEditMode: mockHandleEditMode,
+            });
+
+            render(
+                <Provider store={mockStoreEmptyUser}>
+                    <Header />
+                </Provider>,
+                { wrapper: MemoryRouter }
+            );
+            const backButtonElement = screen.queryByRole('link');
+            expect(backButtonElement).toBeNull();
+        });
+    });
+    describe('when press back button', () => {
+        const mockPreloadState = createPreloadedState(
+            mockUser,
+            mockRoutinesNotEditing,
+            mockDefaultExercises
+        );
+        const mockStore = createStoreMock(mockPreloadState);
+
+        test('and edit mode is off', () => {
+            (useUser as jest.Mock).mockReturnValue({
+                userState: {
+                    user: userMock,
+                },
+            });
+
+            (useRoutines as jest.Mock).mockReturnValue({
+                routinesState: mockRoutinesNotEditing,
+                handleEditMode: mockHandleEditMode,
+            });
+
+            render(
+                <Provider store={mockStore}>
+                    <Header />
+                </Provider>,
+                { wrapper: MemoryRouter }
+            );
+
+            const backButtonElement = screen.getByRole('link');
+            expect(backButtonElement).toBeInTheDocument();
+            fireEvent.click(backButtonElement);
+            expect(mockHandleEditMode).toHaveBeenCalledTimes(0);
+        });
     });
 
-    test('displays back button when user is logged in', () => {
-        const mockUserState = {
-            user: {
-                uid: 'mock-user-id',
-            },
-        };
-        jest.mock('../../hooks/use.user', () => ({
-            useUser: () => ({
-                userState: mockUserState,
-            }),
-        }));
-        render(<Header />);
-        const backButtonElement = screen.getByRole('button');
-        expect(backButtonElement).toBeInTheDocument();
+    describe('when the user press back button', () => {
+        const mockPreloadState = createPreloadedState(
+            mockUser,
+            mockRoutinesEditing,
+            mockDefaultExercises
+        );
+        const mockStore = createStoreMock(mockPreloadState);
+
+        test('and edit mode is on', () => {
+            (useUser as jest.Mock).mockReturnValue({
+                userState: {
+                    user: userMock,
+                },
+            });
+
+            (useRoutines as jest.Mock).mockReturnValue({
+                routinesState: mockRoutinesEditing,
+                handleEditMode: mockHandleEditMode,
+            });
+
+            render(
+                <Provider store={mockStore}>
+                    <Header />
+                </Provider>,
+                { wrapper: MemoryRouter }
+            );
+
+            const backButtonElement = screen.getByRole('link');
+            expect(backButtonElement).toBeInTheDocument();
+            fireEvent.click(backButtonElement);
+            expect(mockHandleEditMode).toHaveBeenCalled();
+        });
     });
 });
